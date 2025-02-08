32f38bd3280dff973774fbe5e72ba831976d6389
EggShark <eggshark@eggshark.dev>
2025-02-07 20:25:14 -0700

    starting on serial port

--- src/device.rs
+++ src/device.rs
@@ -1,5 +1,21 @@
-pub struct Device {
+use serialport::SerialPort;
 
+pub struct Device<T: SerialPort> {
+    port: T,
+    pub last_error_flag: u32,
+    pub slave_adress: u32,
+}
+
+impl<T: SerialPort> Device<T> {
+    pub fn new(mut serial_port: T, slave_adress: u32) -> Self {
+        let _ = serial_port.set_timeout(std::time::Duration::from_millis(200));
+
+        Self {
+            port: serial_port,
+            last_error_flag: 0,
+            slave_adress,
+        }
+    }
 }
 
 #[repr(u8)]
